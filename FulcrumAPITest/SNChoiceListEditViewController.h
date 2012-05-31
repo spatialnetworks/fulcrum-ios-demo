f5e73ed3fbf97b1105285e373d45d0de5f58aaed
Ben Rigas <ben@spatialnetworks.com>
2012-05-31 10:07:59 -0400

    renamed project

---
+++ FulcrumAPITest/SNChoiceListEditViewController.h
@@ -0,0 +1,13 @@
+//
+//  SNChoiceListEditViewController.h
+//  FulcrumAPITest
+//
+//  Created by Ben Rigas on 5/31/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SNChoiceListEditViewController : UIViewController
+
+@end
---
+++ FulcrumAPITest/SNChoiceListEditViewController.m
@@ -0,0 +1,44 @@
+//
+//  SNChoiceListEditViewController.m
+//  FulcrumAPITest
+//
+//  Created by Ben Rigas on 5/31/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "SNChoiceListEditViewController.h"
+
+@interface SNChoiceListEditViewController ()
+
+@end
+
+@implementation SNChoiceListEditViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
